9ddb64a34a407a9fcc3b3a8d80a93ed7b3238907
farah sabour <farahalaaa64@gmail.com>
2024-02-22 18:32:09 +0200

    hello

---
+++ week one/q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ printf(" //// my name is : Farah Sabour \\\\ \n ");
+ printf(" '''''' ECE department lvl 100 '''''' \n ");
+  printf(" \"\"\" mansoura university \"\"\" \n ");
+
+
+    return 0;
+}
